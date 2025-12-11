764765ba32a9fc8c08aa1ee06e50989c4d70931c
AntoBadami <cantobadami@gmail.com>
2025-12-11 19:25:16 -0300

    add: pantalla de inicio

--- gastrack/components.d.ts
+++ gastrack/components.d.ts
@@ -12,6 +12,7 @@ export {}
 declare module 'vue' {
   export interface GlobalComponents {
     AppFooter: typeof import('./src/components/AppFooter.vue')['default']
+    EstadisticasOrdenes: typeof import('./src/components/EstadisticasOrdenes.vue')['default']
     EstadoChip: typeof import('./src/components/EstadoChip.vue')['default']
     Filtro: typeof import('./src/components/Filtro.vue')['default']
     HelloWorld: typeof import('./src/components/HelloWorld.vue')['default']
