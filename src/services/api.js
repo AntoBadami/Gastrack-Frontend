d954fca73db6d518f3861457550abbccf1bd0bfc
fabribiondi2002 <lbiondi733@alumnos.iua.edu.ar>
2025-12-16 18:18:49 -0300

    hotfix: cambio de ip del back

--- src/services/api.js
+++ src/services/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'http://143.0.100.211:443/api/v1',
 })
 
 // Agregar el token automáticamente
